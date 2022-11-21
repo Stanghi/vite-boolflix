dac357d790fbd01ec29e669055db86af2b844954
Stanghi <stangherlin.matti@gmail.com>
2022-11-21 11:28:45 +0100

    init Milestone 0

---
+++ src/components/AppCard.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+    name: 'AppCard',
+};
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'AppMain',
+};
+</script>
+
+<template>
+    <header>M</header>
+</template>
+
+<style lang="scss" scoped></style>

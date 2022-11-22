b18b5fee5167ae36cf94658e351f3143c23502e2
Stanghi <stangherlin.matti@gmail.com>
2022-11-22 16:31:35 +0100

    add carousel at AppTranding

--- src/components/AppTranding.vue
+++ src/components/AppTranding.vue
@@ -9,50 +9,115 @@ export default {
     data() {
         return {
             store,
+            counter: 0,
         };
+    },
+    methods: {
+        autoCarousel() {
+            this.autoPlay = setInterval(() => {
+                this.sliderNextPrev(true);
+            }, 5000);
+        },
+
+        stopIntervall() {
+            clearInterval(this.autoPlay);
+        },
+
+        sliderNextPrev(isNext) {
+            !isNext ? this.counter-- : this.counter++;
+
+            if (this.counter === 20) this.counter = 0;
+            else if (this.counter < 0) this.counter = 20 - 1;
+        },
+    },
+    mounted() {
+        this.autoCarousel();
     },
 };
 </script>
 
 <template>
-    <h3>Di tendenza</h3>
     <div v-if="store.isLoaded">
-        <ul v-for="(el, index) in store.resultsTranding" :key="index">
-            <img
-                :src="'https://image.tmdb.org/t/p/w300/' + el.backdrop_path"
-                :alt="el.title || el.name"
-            />
-            <li><strong>title = </strong>{{ el.title || el.name }}</li>
-            <li><strong>original_title = </strong>{{ el.original_title || el.original_name }}</li>
-            <li>
-                <span v-if="el.original_language === 'en'" class="fi fi-us"></span>
-                <span v-else-if="el.original_language === 'ja'" class="fi fi-jp"></span>
-                <span
-                    v-else-if="el.original_language"
-                    :class="'fi fi-' + el.original_language"
-                ></span>
-                <span v-else class="fi fi-xx"></span>
-            </li>
-            <li>
-                <p v-for="(x, index) in Math.ceil(el.vote_average / 2)" :key="index">
-                    <i class="fa-solid fa-star"></i>
-                </p>
-            </li>
-        </ul>
+        <div v-for="(el, index) in store.resultsTranding" :key="index">
+            <div class="carousel-inner">
+                <div class="carousel-item" :class="index === this.counter && 'active'">
+                    <img
+                        class="d-block w-100"
+                        :src="'https://image.tmdb.org/t/p/original/' + el.backdrop_path"
+                        :alt="el.title || el.name"
+                    />
+                    <h2 class="title">{{ el.title || el.name }}</h2>
+                </div>
+            </div>
+        </div>
+        <div
+            class="circle prev"
+            @click="sliderNextPrev(false)"
+            @mouseover="stopIntervall()"
+            @mouseleave="autoCarousel()"
+        >
+            <i class="fa-solid fa-chevron-left"></i>
+        </div>
+        <div
+            class="circle next"
+            @click="sliderNextPrev(true)"
+            @mouseover="stopIntervall()"
+            @mouseleave="autoCarousel()"
+        >
+            <i class="fa-solid fa-chevron-right"></i>
+        </div>
     </div>
     <AppLoading v-else />
 </template>
 
 <style lang="scss" scoped>
-ul {
+.carousel-inner {
+    position: relative;
+
+    .carousel-item {
+        height: calc(100vh - 30px);
+
+        img {
+            object-fit: contain;
+        }
+
+        .title {
+            position: absolute;
+            bottom: 50px;
+            left: 50px;
+            font-size: 3rem;
+            color: white;
+            text-transform: uppercase;
+            text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;
+        }
+    }
+}
+
+.circle {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 50%;
+    width: 45px;
+    height: 45px;
+    transform: translate(0, -50%);
     padding: 10px;
-    border: 1px solid black;
+    border-radius: 50%;
+    cursor: pointer;
+    background-color: rgba(0, 0, 0, 0.3);
 }
-h2,
-h3 {
-    text-align: center;
+
+.circle.next {
+    color: white;
+    right: 5%;
 }
-p {
-    display: inline-block;
+.circle.prev {
+    color: white;
+    left: 5%;
+}
+
+.circle:hover {
+    background-color: rgba(0, 0, 0, 0.5);
 }
 </style>
